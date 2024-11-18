7e61d31b64ac00e18c1b081ffd2e993fb019eaff
anasmith94 <kikio16@msn.com>
2024-11-18 10:00:53 +0100

    Add files via upload

---
+++ Procesos por lotes/mkprofile.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Script mkprofile.bat
+Rem Autor: Ana Luisa Vargas Smith
+Rem Fecha:13/11/2024
+mkdir %USERPROFILE%\asir %USERPROFILE%\daw %USERPROFILE%\ceti %USERPROFILE%\smr
+mkdir %USERPROFILE%\asir\doc  %USERPROFILE%\asir\pract %USERPROFILE%\asir\examen
+mkdir %USERPROFILE%\daw\doc  %USERPROFILE%\daw\pract %USERPROFILE%\daw\examen
+mkdir %USERPROFILE%\ceti\doc  %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\examen
+mkdir %USERPROFILE%\smr\doc  %USERPROFILE%\smr\pract %USERPROFILE%\smr\examen
